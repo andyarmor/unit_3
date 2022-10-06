ba3fcbc1637b9d6a33e303d7d8ce198d8399f308
andyarmor <aoarmor25@students.bmchs.org>
2022-10-06 16:38:43 -0500

    done but confused

--- Lab 3.02.py
+++ Lab 3.02.py
@@ -8,10 +8,21 @@ Create a function, birthday_song, that prints out the happy birthday song to wha
 
   # Name: birthday_song
   # Purpose: birthday_song prints out a personalized birthday song
-  # Arguments: name, string
+  # Arguments: name, string     #your code goes here
+
   # Returns: none
-  def birthday_song(name):
-     #your code goes here
+
+birthday_song_name = input("What's your name?")
+def birthday_song(name):
+    print("Happy birthday to " + name)
+    print("Happy birthday to " + name)
+    print("Happy birthday to " + name + " happy birthday to you!")
+
+    return
+
+birthday_song(birthday_song_name)
+
+    
 
 Lab Exercise 2
 ---------------
@@ -30,10 +41,21 @@ What is different about passing in a list as an argument?
 Read about list aliasing in section 3.4 of the associated reading, and write down what is happening in this case.
 Remember, the associated reading is in the "SWBAT" section on moodle!
 '''
+
 import random
+def random_card_picker():
+    
+    number = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-#def five_cards():
-    #card_options = random.choice(['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'])
-    #print(card_options)
+    card1 = number[random.randint(0,12)] + " "
+    card2 = number[random.randint(0,12)] + " "
+    card3 = number[random.randint(0,12)] + " "
+    card4 = number[random.randint(0,12)] + " "
+    card5 = number[random.randint(0,12)] + " "
 
+    print("The cards picked in order are: " + card1 + card2 + card3 + card4 + card5)
+
+    return
+
+random_card_picker()
 
